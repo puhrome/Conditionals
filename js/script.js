b3ea9883453a9aa14a5b0d8b3fb608cf6a38050b
puhrome <phirom.yim@fullsail.edu>
2014-04-14 12:34:36 -0400

    Basic Conditional Logic
    Using WebStorm to perform basic conditional statements using true or
    false and if then statements.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ *Conditional Logic Concept Created by Phirom Yim on 4/14/14.
+ */
+var oldEnough = false;
+
+//if the child is old enough, print to the console, you can ride
+
+if(oldEnough){
+    //code performed if condition is true
+    //condition goes inside of ( )
+    //code block goes inside of { }
+    console.log("You can ride the coaster")
+}
